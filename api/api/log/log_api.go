3bbb38a479ca3478c752f5a479cdc1e2da84860b
fedehsq <fedebernacca@gmail.com>
2022-12-07 16:05:24 +0100

    es client query fixed, to redefine log route

--- api/api/log/log_api.go
+++ api/api/log/log_api.go
@@ -3,7 +3,6 @@ package logapi
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/fedehsq/api/api"
 	"net/http"
 	"strings"
@@ -22,8 +21,8 @@ import (
 // @Router       /v1/log/get-logs [get]
 // @Security 	 JWT
 func GetAll(w http.ResponseWriter, r *http.Request) {
-	ok,identity, err := api.VerifyToken(r)
-	
+	ok, identity, err := api.VerifyToken(r)
+
 	if err != nil {
 		api.WriteLog("GET", "/v1/log/get-logs", "Unauthorized user", r.RemoteAddr, "")
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -36,11 +35,11 @@ func GetAll(w http.ResponseWriter, r *http.Request) {
 	}
 	api.WriteLog("GET", "/v1/log/get-logs", identity, r.RemoteAddr, "")
 
-	body := fmt.Sprintf(
-		`{"query": {"match_all": {}},"sort": [{"time": {"order": "desc"}}]}`)
+	body :=
+		`{"query": {"match_all": {}}}`
 	res, err := config.EsClient.Search(
 		config.EsClient.Search.WithContext(context.Background()),
-		config.EsClient.Search.WithIndex("logs"),
+		//config.EsClient.Search.WithIndex("logs"),
 		config.EsClient.Search.WithBody(strings.NewReader(body)),
 		config.EsClient.Search.WithPretty(),
 	)
